2ad5e01d8ac89010b7f8cb53914063aa37ab318a
XecellencWeb <128531293+XecellencWeb@users.noreply.github.com>
2023-09-21 17:33:54 +0100

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -6,8 +6,7 @@ export const sendErr = (res,err,mes)=>{
 
 export const verifyToken = async(req,res,next)=>{
     const token = req.params.token
-    console.log(token)
-    if(!token) return sendErr(res,401,'You are not authenticated')
+    if(!token || token === 'undefined') return sendErr(res,401,'You are not authenticated')
     const user = jwt.verify(token,process.env.token)
     req.user = user
      next()
